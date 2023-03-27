c64dcfe563c9f5a85761430d2253ebe345160a38
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2023-03-27 09:42:31 +0200

    Add copilot

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -47,6 +47,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- Copilot
+  'github/Copilot.vim',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
