9d90a59a72a0dba636bf1e6a343e682e20796b60
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2024-02-02 17:01:33 +0100

    change to gitsigns

--- neovim/.config/nvim/lua/custom/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/custom/plugins/gitsigns.lua
@@ -1,15 +1,9 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = {"BufReadPre", "BufNewFile"},
-  config = true,
-  opts = {
-    -- See `:help gitsigns.txt`
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+   "lewis6991/gitsigns.nvim",
+   event = { "BufReadPre", "BufNewFile" },
+   config = true,
+   opts = {
+      -- See `:help gitsigns.txt`
+      current_line_blame = true,
+   },
 }
