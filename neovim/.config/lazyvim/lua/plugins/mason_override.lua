7be024efdba581a4b9ecfb19d30f92799d90ea0b
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2025-07-16 10:18:57 +0200

    Override default Mason version in LazyVim

---
+++ neovim/.config/lazyvim/lua/plugins/mason_override.lua
@@ -0,0 +1,13 @@
+-- This file is used to override the default Mason configuration in LazyVim.
+-- When LazyVim removes the version pinning, this file will no longer be required.
+return {
+  {
+    "mason-org/mason.nvim",
+    version = "^2.0.0",
+    -- opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^2.0.0",
+  },
+}
